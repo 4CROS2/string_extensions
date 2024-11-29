57ab4b2f0e895c6a8707e9d9ee54c5d7c3552b23
yeferson rivas <yeferok2@gmail.com>
2024-11-29 12:58:22 -0500

    firebase extensions impl

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,3 +1,6 @@
 library extensions;
 
 export 'src/string_extensions.dart';
+export 'src/document_snapshot_extension.dart';
+export 'src/document_snapshot_stream_extension.dart';
+export 'src/querysnapshot_to_json_extension.dart';
