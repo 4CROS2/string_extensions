18212140248a3d9c2cf58c3685d9317f9e3e5c7f
yeferson rivas <yeferok2@gmail.com>
2024-11-29 13:16:05 -0500

    updated

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,7 +1,6 @@
-library extensions;
+library;
 
 export 'src/string_extensions.dart';
-
 export 'src/document_snapshot_extension.dart';
 export 'src/document_snapshot_stream_extension.dart';
 export 'src/querysnapshot_to_json_extension.dart';
