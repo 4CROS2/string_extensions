dcac50e976d3215b7e394aa1bab94c908be24444
yeferson rivas <yeferok2@gmail.com>
2025-04-15 21:22:53 -0500

    refactor: clean up string extension exports and improve test readability

--- lib/src/string_extensions.dart
+++ lib/src/string_extensions.dart
@@ -8,7 +8,7 @@ extension StringExtensions on String {
     }).join(' ');
   }
 
-   String capitalize() {
+  String capitalize() {
     if (isEmpty) return '';
 
     final regExp = RegExp(r'[A-Za-zÁÉÍÓÚáéíóúÑñ]');
--- test/extensions_test.dart
+++ test/extensions_test.dart
@@ -2,7 +2,6 @@
 import 'package:extensions/extensions.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-
 void main() {
   group('Pruebas de la extensión capitalize', () {
     test('Devuelve cadena vacía si la cadena es vacía', () {
@@ -13,7 +12,9 @@ void main() {
       expect('hola'.capitalize(), 'Hola');
     });
 
-    test('No modifica los caracteres que no son letras al inicio (ejemplo con signo)', () {
+    test(
+        'No modifica los caracteres que no son letras al inicio (ejemplo con signo)',
+        () {
       expect('¿quieres cerrar?'.capitalize(), '¿Quieres cerrar?');
     });
 
@@ -21,11 +22,15 @@ void main() {
       expect('123!'.capitalize(), '123!');
     });
 
-    test('Capitaliza correctamente cuando la primera letra es minúscula y el resto está en mayúsculas', () {
+    test(
+        'Capitaliza correctamente cuando la primera letra es minúscula y el resto está en mayúsculas',
+        () {
       expect('aREA'.capitalize(), 'Area');
     });
 
-    test('Mantiene la parte no alfabética intacta y capitaliza solo el primer carácter alfabético', () {
+    test(
+        'Mantiene la parte no alfabética intacta y capitaliza solo el primer carácter alfabético',
+        () {
       expect('\$\$\$dollar'.capitalize(), '\$\$\$Dollar');
     });
   });
